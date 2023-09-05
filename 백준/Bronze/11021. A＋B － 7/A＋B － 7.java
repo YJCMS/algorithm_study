61694f894a4dc10ecb1686bffeb54a313d7a65cd
YJCMS <101327582+YJCMS@users.noreply.github.com>
2023-09-05 16:16:21 +0900

    [Bronze V] Title: A+B - 7, Time: 268 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int t = in.nextInt();
+
+        for(int i = 1; i <= t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println("Case #" + i + ": " + (a + b));
+        }
+        in.close();
+    }
+}
