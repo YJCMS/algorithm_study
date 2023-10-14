ea27064a6a7809272be84b92b09617c1e89f5d74
YJCMS <101327582+YJCMS@users.noreply.github.com>
2023-10-14 14:32:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char a = in.next().charAt(0);
+        int ascii = (int) a;
+        System.out.println(ascii);
+    }
+}
