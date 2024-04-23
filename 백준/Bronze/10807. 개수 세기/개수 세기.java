26901b1232405f96c6659bbfc052f9a84d289635
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-04-23 19:40:24 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         for(int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         int result = 0;
-        int v = in.nextInt();
-        
+        int v = Integer.parseInt(br.readLine());
+
         for(int i = 0; i < n; i++) {
             if(arr[i] == v) result++;
         }
-        
+
         System.out.println(result);
     }
 }
