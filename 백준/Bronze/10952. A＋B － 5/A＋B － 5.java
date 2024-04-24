416427f97e00d8650be94a809b87227861da3f80
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-04-24 16:25:46 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while(true) {
-            int a = in.nextInt();
-            int b = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             if(a == 0 && b == 0) break;
-
-            System.out.println(a+b);
+            sb.append(a+b).append('\n');
         }
+        
+        System.out.println(sb);
     }
 }
