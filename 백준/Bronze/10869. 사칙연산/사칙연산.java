5e2dad376d3c9638876ace640b9d99f0044743bc
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-04-27 20:06:38 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        System.out.println(A+B);
-        System.out.println(A-B);
-        System.out.println(A*B);
-        System.out.println(A/B);
-        System.out.println(A%B);
-        in.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(a+b).append('\n')
+                .append(a-b).append('\n').append(a*b).append('\n')
+                .append(a/b).append('\n').append(a%b).append('\n');
+
+        System.out.println(sb);
     }
 }
