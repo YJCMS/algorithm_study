9a481a8987188035c30bcaa1c79eeb496cdd24e4
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-01 17:29:29 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        String result = "";
-        for(int i = 0; i < n; i++) {
-            result += "*";
-            System.out.println(result);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i ++) {
+            for(int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
         }
-        in.close();
+
+        System.out.println(sb);
     }
 }
