7f296a6931d8ff287929669c226153d28c220d86
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-12 13:58:27 +0900

    [Bronze V] Title: A+B - 3, Time: 136 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        int T = in.nextInt();
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+    
         for (int i = 0; i < T; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            System.out.println(a+b);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append('\n');
         }
+        System.out.println(sb);
     }
 }
