47923112fe7e1a927cfc1f1e7c76d5e1bbfa05d8
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-13 21:29:50 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,21 +1,21 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        ArrayList<Integer> arrayList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int max = 0;
-        int idx = 0;
-
-        for (int i=0; i< 9; i++) {
-            arrayList.add(in.nextInt());
-            if(arrayList.get(i) > max) {
-                max = arrayList.get(i);
-                idx = i+1;
+        int index = 0;
+        for(int i = 1; i < 10; i++) {
+            int a =Integer.parseInt(br.readLine());
+            if(max < a) {
+                max = a;
+                index = i;
             }
         }
-        System.out.println(max);
-        System.out.println(idx);
+        sb.append(max).append('\n').append(index).append('\n');
+        System.out.println(sb);
     }
 }
