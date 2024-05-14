4b460a627c7e46970aa27cca05e001f17951c78c
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-14 22:06:25 +0900

    [Bronze III] Title: 오븐 시계, Time: 152 ms, Memory: 16072 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        int C = in.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
+        
         A += C/60;
         B += C%60;
-
+        
         if (B >= 60) {
             A += 1;
             B -= 60;
         }
-
         if (A >= 24) {
             A -= 24;
         }
-
+        
         System.out.println(A + " " + B);
-
     }
 }
