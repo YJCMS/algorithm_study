971bf2479c501999ed22925cb46e19c85778c67d
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-15 22:48:22 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int i = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
         String str = "";
+        StringBuilder sb = new StringBuilder();
 
         for(int j = 0; j < i; j++){
-            str = in.next();
-            System.out.print(str.charAt(0));
-            System.out.println(str.charAt(str.length()-1));
+            str = br.readLine();
+            sb.append(str.charAt(0)).append(str.charAt(str.length()-1)).append('\n');
         }
+        System.out.println(sb);
     }
 }
