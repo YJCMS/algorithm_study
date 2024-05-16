738cc4b8d6b60ab47d1f77b1eda512a486bad0b1
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-16 21:54:34 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int[] arr = new int[30];
-        
+
         for(int i = 0; i < 28; i++) {
-            int j = in.nextInt();
-            arr[j-1] = j;
+            int a = Integer.parseInt(br.readLine());
+            arr[a-1] = 1;
         }
-        for(int k = 0; k < 30; k++) {
-            if(arr[k] == 0) System.out.println(k+1);
-        }
+
+       for(int i = 0; i < 30; i++) {
+           if(arr[i] == 0) {
+               sb.append(i+1).append('\n');
+           }
+       }
+
+        System.out.println(sb);
     }
 }
