765850df02dd6fb7b167743ba560a99dbc6f9d60
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-17 18:09:17 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 1; k <= 2*i-1; k++) {
+                sb.append("*");
+            }
+            sb.append('\n');
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j <= i; j++) {
+                sb.append(" ");
+            }
+            for(int k = 2*(n-i)-1; k > 0 ; k--) {
+                sb.append("*");
+            }
+            sb.append('\n');
+        }
+        
+        System.out.println(sb);
+    }
+}
