4471abfad20f7d4ba8825e153ac86365c0768342
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-05-23 21:58:21 +0900

    [Bronze II] Title: 상수, Time: 160 ms, Memory: 16228 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,24 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
-       String s = in.nextLine();
-       String a = "";
-       String b = "";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
 
-       for(int i = 2; i >= 0; i-- ) {
-           a = a + s.charAt(i);
-           b = b + s.charAt(i+4);
-       }
+        String a = "";
+        String b = "";
 
-       int result1 = Integer.parseInt(a);
-       int result2 = Integer.parseInt(b);
+        for(int i = 2; i >= 0; i-- ) {
+            a = a + s.charAt(i);
+            b = b + s.charAt(i+4);
+        }
 
-       if (result1 < result2) {
-           System.out.println(result2);
-       } else {
-           System.out.println(result1);
-       }
+        int result1 = Integer.parseInt(a);
+        int result2 = Integer.parseInt(b);
+
+        if (result1 < result2) {
+            System.out.println(result2);
+        } else {
+            System.out.println(result1);
+        }
     }
 }
