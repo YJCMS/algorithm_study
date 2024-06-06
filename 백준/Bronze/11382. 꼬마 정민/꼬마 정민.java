2740c5373fe70f28f3065b05bbeab40298141538
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-06-06 15:38:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        long A = in.nextLong();
-        long B = in.nextLong();
-        long C = in.nextLong();
-        System.out.println(A+B+C);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(Long.parseLong(st.nextToken())
+                           + Long.parseLong(st.nextToken())
+                           + Long.parseLong(st.nextToken()));
     }
 }
