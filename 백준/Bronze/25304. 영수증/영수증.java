5966246337b4197377d79524eaf8c3b3df3b6dad
YJCMS <101327582+YJCMS@users.noreply.github.com>
2024-06-16 22:21:33 +0900

    [Bronze IV] Title: 영수증, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        int n = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         int result = 0;
 
-        for (int i = 0; i < n; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             result += a * b;
         }
 
-        if (result == x) System.out.println("Yes");
-        else System.out.println("No");
-
+        if(result == m) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
