787bb4d3222498c24c110ac20cf3585daf206cc1
이승현 <101327582+YJCMS@users.noreply.github.com>
2024-06-30 23:30:24 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,15 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         double[] arr = new double[n];
         double max = 0;
         double result = 0;
 
         for(int i = 0; i < n; i++) {
-            arr[i] = in.nextLong();
+            arr[i] = Long.parseLong(st.nextToken());
             if(arr[i] > max) max = arr[i];
         }
 
