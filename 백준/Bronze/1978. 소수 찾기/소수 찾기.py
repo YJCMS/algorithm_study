d24b896a376ee2ed97aa0351c2bb776ec996171e
이승현 <101327582+YJCMS@users.noreply.github.com>
2024-07-06 10:19:30 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,11 @@
+n = int(input())
+list_n = list(map(int, input().split()))
+result = 0
+for num in list_n:
+    count = 0
+    for i in range(1, num+1):
+        if num%i == 0:
+            count += 1
+    if count == 2 :
+        result += 1
+print(result)
