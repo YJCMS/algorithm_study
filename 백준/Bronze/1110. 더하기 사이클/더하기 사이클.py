0a7529440e77bad05bb257c4105cfcf46c90f39c
이승현 <101327582+YJCMS@users.noreply.github.com>
2024-07-11 10:27:58 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+num = n
+cnt = 0
+
+while True:
+    a = num//10
+    b = num%10
+    c = (a+b)%10
+    num = b*10+c
+    cnt += 1
+    if num == n:
+        break
+
+print(cnt)
