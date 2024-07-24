0517228c1e377429228ee169fc5337e0c9754eb0
이승현 <101327582+YJCMS@users.noreply.github.com>
2024-07-24 22:58:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 156 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if num[i] > num[j]:
+            dp[i] = max(dp[i], dp[j] + 1)  
+                  
+print(max(dp))
