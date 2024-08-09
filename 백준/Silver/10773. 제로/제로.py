8cb78fb588d88f81d9231a8020e2e05b67d27210
이승현 <101327582+YJCMS@users.noreply.github.com>
2024-08-09 10:56:46 +0900

    [Silver IV] Title: 제로, Time: 76 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+result = []
+
+for i in range(n):
+    a = int(input())
+    if a != 0 :
+        result.append(a)
+    else :
+        result.pop()
+
+print(sum(result))
