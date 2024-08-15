718b7693409e4b7d6a08f53d230c7688be469c57
이승현 <101327582+YJCMS@users.noreply.github.com>
2024-08-15 10:42:12 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 72 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,19 +1,21 @@
 import sys
 from collections import deque
 
-n, m = map(int, sys.stdin.readline().split())
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
 queue = deque()
 result = []
 
-for i in range(n):
-    queue.append(i+1)
-
+for i in range(1, n+1):
+    queue.append(i)
+    
 while queue:
-    for i in range(m-1):
+    for i in range(k-1):
         queue.append(queue.popleft())
     result.append(queue.popleft())
-
+    
 print("<", end="")
 print(*result, sep=', ',end="")
 print(">")
-    
