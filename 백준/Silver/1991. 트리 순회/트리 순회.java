72b106dbf97fba85d045758345ab40dada065e46
이승현 <101327582+YJCMS@users.noreply.github.com>
2025-01-22 15:00:18 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Node {
+    char value;
+    Node left;
+    Node right;
+
+    public Node(char value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class Main {
+    static Node[] tree;
+
+    public static void preorder(Node node) {
+        if(node == null) return;
+        System.out.print(node.value);
+        preorder(node.left);
+        preorder(node.right);
+    }
+    public static void inorder(Node node) {
+        if(node == null) return;
+        inorder(node.left);
+        System.out.print(node.value);
+        inorder(node.right);
+    }
+    public static void postorder(Node node) {
+        if(node == null) return;
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        tree = new Node[n+1];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char root = st.nextToken().charAt(0);
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
+
+            if(tree[root - 'A'] == null) {
+                tree[root - 'A'] = new Node(root);
+            }
+            if(left != '.') {
+                tree[left - 'A'] = new Node(left);
+                tree[root - 'A'].left = tree[left - 'A'];
+            }
+            if(right != '.') {
+                tree[right - 'A'] = new Node(right);
+                    tree[root - 'A'].right = tree[right - 'A'];
+            }
+        }
+        preorder(tree[0]);
+        System.out.println();
+        inorder(tree[0]);
+        System.out.println();
+        postorder(tree[0]);
+        System.out.println();
+    }
+}
